a3e1c467a628dd430e3b47aefb82c80a3a1973ae
David Giraldo <davidgiraldobueno@gmail.com>
2024-07-05 00:18:28 -0500

    Add initial tests for user list

---
+++ src/__tests__/components/UserList.test.js
@@ -0,0 +1,32 @@
+import { screen, render, within } from "@testing-library/react";
+import UserList from "../../components/UserList";
+
+describe('User List component', () => {
+    const mockUsers = [{ name: 'test1', email: 'test1@test.com' }, { name: 'test2', email: 'test2@test.com' }]
+    it('should render on the screen', () => {
+        render(<UserList users={[]} />);
+
+        const userList = screen.getByRole('table');
+
+        expect(userList).toBeInTheDocument();
+    })
+
+    it('should render one row per user', () => {
+        const { container } = render(<UserList users={mockUsers} />);
+
+        const users = screen.getAllByTestId('userRow');
+        const rows = within(screen.getByTestId('usersTBody')).getAllByRole('row');
+        const rows2 = container.querySelectorAll('tbody tr');
+
+        expect(users).toHaveLength(2);
+        expect(rows).toHaveLength(2);
+        expect(rows2).toHaveLength(2);
+
+    })
+
+    it('should render the name and email of each user', () => {
+        render(<UserList users={mockUsers} />);
+        // screen.logTestingPlaygroundURL();
+
+    })
+})
