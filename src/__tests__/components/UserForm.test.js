db455564c3e317c3f6efa59224dc9ae652bfa4c2
David Giraldo <davidgiraldobueno@gmail.com>
2024-07-05 14:52:52 -0500

    add more tests for the form

--- src/__tests__/components/UserForm.test.js
+++ src/__tests__/components/UserForm.test.js
@@ -12,7 +12,7 @@ describe('User Form Component', () => {
 
         expect(inputs).toHaveLength(2);
         expect(button).toBeInTheDocument;
-    })
+    });
 
     it('should call onUserAdd when form is submitted', async () => {
         const mock = jest.fn();
@@ -35,5 +35,28 @@ describe('User Form Component', () => {
 
         expect(mock).toHaveBeenCalled();
         expect(mock).toHaveBeenCalledWith({ name: 'John Doe', email: 'john@john.com' });
-    })
+    });
+
+    it('should empties two inputs after the form is submitted', async () => {
+        const mock = jest.fn();
+
+        render(<UserForm onUserAdd={mock} />);
+
+        const nameInput = screen.getByRole('textbox', { name: /name/i });
+        const emailInput = screen.getByRole('textbox', { name: /email/i });
+
+        await user.click(nameInput);
+        await user.keyboard('John Doe');
+
+
+        await user.click(emailInput);
+        await user.keyboard('john@john.com');
+
+
+        const button = screen.getByRole('button')
+        await user.click(button);
+
+        expect(nameInput).toHaveValue('');
+        expect(emailInput).toHaveValue('');
+    });
 })
